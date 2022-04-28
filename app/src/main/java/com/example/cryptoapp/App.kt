52fd119bd0ab0eddd907dd1f5f74591dfa08511a
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-04-28 17:41:08 +0300

    add dependencies for single work manager

--- app/src/main/java/com/example/cryptoapp/App.kt
+++ app/src/main/java/com/example/cryptoapp/App.kt
@@ -1,12 +1,32 @@
 package com.example.cryptoapp
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerAppComponent
+import javax.inject.Inject
 
-class App: Application() {
+class App: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: RefreshDataWorkerFactory
 
     val component by lazy {
         DaggerAppComponent.factory()
             .create(this)
+    }
+
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
     }
 }
--- app/src/main/java/com/example/cryptoapp/di/AppComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/AppComponent.kt
@@ -1,17 +1,21 @@
 package com.example.cryptoapp.di
 
 import android.app.Application
+import com.example.cryptoapp.di.annotation.ApplicationScope
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
 import dagger.BindsInstance
 import dagger.Component
 
+@ApplicationScope
 @Component(modules = [ViewModelModule::class, DataModule::class])
 interface AppComponent {
 
     fun inject(coinListActivity: CoinPriceListActivity)
 
     fun inject(coinDetailFragment: CoinDetailFragment)
+
+    fun inject(application: Application)
 
     @Component.Factory
     interface AppComponentFactory{
